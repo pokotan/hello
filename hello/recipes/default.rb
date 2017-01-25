aec6661fbfcb70d6bd3f4a4e13fa55c6f1c7589c
OKAMOTO,Nau <nau-okamoto@dac.co.jp>
2017-01-26 02:13:02 +0900

    updated

--- hello/recipes/default.rb
+++ hello/recipes/default.rb
@@ -1,3 +1,13 @@
-file "/tmp/hello.txt" do
-	  action :create
+group 'nsqgroup' do
+	action :create
+	gid '666'
+end
+
+user 'nsquser' do
+	action :create
+	comment 'NSQ User'
+	uid '666'
+	gid '666'
+	home '/home/nsquser'
+	shell '/sbin/nologin'
 end
